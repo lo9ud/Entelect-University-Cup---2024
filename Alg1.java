08cc99ed48ebe3dac4b8749e76239cc627d43570
lo9ud <adam.dkent@icloud.com>
2024-07-27 12:09:00 +0200

    Create back-propogation algo class

--- Alg1.java
+++
@@ -1,3 +0,0 @@
-public class Alg1 {
-
-}
---
+++ BackPropogationPathFinder.java
@@ -0,0 +1,16 @@
+public class BackPropogationPathFinder extends PathFinder {
+
+    @Override
+    public Path findPath() {
+        Path path = new Path();
+        // Find best straight-line path
+        
+
+        // Look for best transform for path
+
+        // Repeat ad infinitum until path length exceeds limit
+
+        return path;
+        }
+
+}
