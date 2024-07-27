ee53e55b2723be46cb6b0f6f748ed9e7d05ac974
lo9ud <adam.dkent@icloud.com>
2024-07-27 12:08:41 +0200

    Update interface

--- PathFinder.java
+++ PathFinder.java
@@ -1,6 +1,22 @@
 public abstract class PathFinder {
     public abstract Path findPath();
 
+    static final int[] BIOME_VALUE_PER_DAY = {
+        1,
+        14,
+        28,
+        42,
+        57,
+        71,
+        85,
+        100,
+    };
+
+    public static float nodeScore(World.Node node, int index) {
+        int days = index / 3 + 1;
+        return node.multiplier * PathFinder.BIOME_VALUE_PER_DAY[node.biome] * days;
+    }
+
     public void displayPath(Path path) {
         for (World.Node node : path.getPath()) {
             System.out.println("Node: " + node.x + ", " + node.y);
